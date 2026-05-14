c75ce6cc67dd18eefd14bbcda76b003a22dc2704
Mateus Pinto <mateusfccp@gmail.com>
2026-05-14 18:40:24 -0300

    Revert "first comits - testing"
    This reverts commit d69aa9f84fb2de8eb77e0039a582af904676363e.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,21 @@
-﻿using UnfathomableParking.Models;
-using UnfathomableParking.Scenes;
+﻿using UnfathomableParking.Interfaces;
 using UnfathomableParking.Services;
 
-var scene = new MainMenuScene();
+var scene = new NoScene();
 var engine = new Engine(scene);
 engine.Start();
-Console.Clear();
+
+internal sealed class NoScene : IScene
+{
+    public void Draw(Engine.Canvas canvas)
+    {
+    }
+
+    public void OnKeyPressed(ConsoleKeyInfo keyInfo)
+    {
+        if (keyInfo.Key != ConsoleKey.None)
+        {
+            Engine.Instance?.Stop();
+        }
+    }
+}
