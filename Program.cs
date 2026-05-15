c150d3271e5d249b11f36f9deed9ead72816fdd5
David <leinerdavidgp@gmail.com>
2026-05-15 17:45:36 -0300

    remove test line

--- Program.cs
+++ Program.cs
@@ -2,8 +2,6 @@
 using UnfathomableParking.Services;
 
 ParkingBeachManager _beachManager = new ParkingBeachManager();
-_beachManager.AddParkingBeach("NiggaBeach",10,4,23.5m); //To test
-
 var scene = new MainMenuScene(_beachManager, 0, 0);
 var engine = new Engine(scene);
 engine.Start();
