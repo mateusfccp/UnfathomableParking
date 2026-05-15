e55a48a670a8d421644053ed37a7c47982b52cfb
Mathias Schneider <mathy.sch.2007@gmail.com>
2026-05-15 12:18:56 -0300

    change formating and implemented sort

--- Program.cs
+++ Program.cs
@@ -2,11 +2,6 @@
 using UnfathomableParking.Services;
 
 ParkingBeachManager _beachManager = new ParkingBeachManager();
-_beachManager.AddParkingBeach(10, 3, 23.5m);
-_beachManager.AddParkingBeach(10, 4, 23m);
-_beachManager.AddParkingBeach(10, 3, 20.4m);
-_beachManager.AddParkingBeach(10, 6, 23.3m);
-_beachManager.AddParkingBeach(10, 7, 26.5m);
 
 var scene = new MainMenuScene(_beachManager, 0, 0);
 var engine = new Engine(scene);
--- Scenes/MainMenuScene.cs
+++ Scenes/MainMenuScene.cs
@@ -6,15 +6,37 @@ using UnfathomableParking.Services;
 
 namespace UnfathomableParking.Scenes;
 
-public class MainMenuScene(ParkingBeachManager beachManager, int headIndex, int selectedFieldIndex) : IScene
+/// <summary>
+/// A class that contains all of the Main Menu UI and logic
+/// </summary>
+public class MainMenuScene : IScene
 {
-    private int _headIndex = headIndex;
-    private int _selectedFieldIndex = selectedFieldIndex;
+    private int _headIndex;
+    private int _selectedFieldIndex;
+    private ParkingBeachManager _beachManager;
+
     private SortingState _sortingState = SortingState.capacity;
-    private List<ParkingBeach> _parkingBeaches => beachManager.ParkingBeaches;
+    private List<ParkingBeach> _sortedBeaches; // default sort = capacity
+    private List<ParkingBeach> _parkingBeaches => _sortedBeaches;
     private List<ParkingBeach>? _visualParkingBeaches;
+
+    /// <summary>
+    /// Gets the parking lot selected from the parking beach list;
+    /// </summary>
     public ParkingBeach? SelectedBeach => _visualParkingBeaches?[_selectedFieldIndex];
 
+    public MainMenuScene(ParkingBeachManager beachManager, int headIndex, int selectedFieldIndex)
+    {
+        _headIndex = headIndex;
+        _selectedFieldIndex = selectedFieldIndex;
+        _sortedBeaches = mergeSort(new List<ParkingBeach>(beachManager.ParkingBeaches), _sortingState);
+        _beachManager = beachManager;
+    }
+
+    /// <summary>
+    /// A scene that displays the Main Menu
+    /// </summary>
+    /// <param name="canvas"></param>
     public void Draw(Engine.Canvas canvas)
     {
         canvas.Clear();
@@ -25,7 +47,6 @@ public class MainMenuScene(ParkingBeachManager beachManager, int headIndex, int 
 
         var defaultStyle = new Style();
         var selectedStyle = new Style(foregroundColor: Color.DodgerBlue);
-
 
         // Title
         canvas.Draw("╷ ╷╭╮╷╭─╴╭─╮╶┬╴╷ ╷╭─╮╭┬╮╭─╮╭╮ ╷  ╭─╴   ╭─╮╭─╮╭─╮╷╭ ╷╭╮╷╭─╴", (uint)(canvas.Width / 2 - 29), originY - 5);
@@ -39,14 +60,14 @@ public class MainMenuScene(ParkingBeachManager beachManager, int headIndex, int 
         // Main Box
         canvas.DrawBox(originX - 2, originY, (uint)width, height);
 
-        // Parking lot list visual - TODO: Display name, revenue and free spots
+        // Parking lot list visual
         if (_parkingBeaches.Count >= 4)
         {
             _visualParkingBeaches = _parkingBeaches[_headIndex..(_headIndex + 4)];
             for (int i = 0; i < 4; i++) // Hacer lista de los que entran (son 4)
             {
                 canvas.DrawBox(originX - 1, (uint)(originY + 1 + i * 5), (uint)width - 2, 5, _selectedFieldIndex == i ? selectedStyle : defaultStyle);
-                canvas.Draw($"{_visualParkingBeaches[i].Width.ToString()}×{_visualParkingBeaches[i].Height.ToString()} parking slots", originX, (uint)(originY + 1 + i * 5) + 2);
+                canvas.Draw($"{"Parking lot name" + ":", -25} {_visualParkingBeaches[i].FreeSlots + " slots", -4} {"$" + _visualParkingBeaches[i].TotalRevenue,-10}", originX, (uint)(originY + 1 + i * 5) + 2);
             }
         }
         else // La lista tiene menos que 4 elementos
@@ -55,7 +76,7 @@ public class MainMenuScene(ParkingBeachManager beachManager, int headIndex, int 
             for (int i = 0; i < _visualParkingBeaches.Count; i++) // Hacer lista completa
             {
                 canvas.DrawBox(originX - 1, (uint)(originY + 1 + i * 5), (uint)width - 2, 5, _selectedFieldIndex == i ? selectedStyle : defaultStyle);
-                canvas.Draw($"{_visualParkingBeaches[i].Width.ToString()}×{_visualParkingBeaches[i].Height.ToString()} parking slots", originX, (uint)(originY + 1 + i * 5) + 2);
+                canvas.Draw($"{"Parking lot name" + ":",-25} {_visualParkingBeaches[i].FreeSlots + " slots", -4} {"$" + _visualParkingBeaches[i].TotalRevenue,-10}", originX, (uint)(originY + 1 + i * 5) + 2); // Temporary format will change name and maybe add colour
             }
         }
 
@@ -94,8 +115,6 @@ public class MainMenuScene(ParkingBeachManager beachManager, int headIndex, int 
             canvas.Draw("█", originX + (uint)width, originY + (uint)(i + sliderHeadIndex), new Style(Color.White, decoration: Decoration.Bold));
         }
 
-
-
         // Create Button
         canvas.DrawBox(originX - 2, originY + height, (uint)(width / 4), 3);
         canvas.Draw("Create (C)", originX - 1, originY + height + 1);
@@ -113,8 +132,12 @@ public class MainMenuScene(ParkingBeachManager beachManager, int headIndex, int 
         canvas.Draw("Sort (S)", originX - 2 + 3 * ((uint)(width / 4)) + 5, originY + height + 1);
     }
 
+    /// <summary>
+    /// Preforms various actions and updates depending on when and what key is pressed
+    /// </summary>
+    /// <param name="keyInfo"></param>
     public void OnKeyPressed(ConsoleKeyInfo keyInfo)
-    { 
+    {
         // Head Index clamp
         if (_parkingBeaches.Count >= 4) _headIndex = Math.Clamp(_headIndex, 0, _parkingBeaches.Count - 4);
         else _headIndex = 0;
@@ -131,25 +154,122 @@ public class MainMenuScene(ParkingBeachManager beachManager, int headIndex, int 
                 if (_selectedFieldIndex == -1 && !(_headIndex == 0)) _headIndex--;
                 break;
             case ConsoleKey.Enter:
-                if (_parkingBeaches != null && _parkingBeaches.Count != 0 && SelectedBeach != null) Engine.Instance?.UpdateScene(new ParkingBeachScene(SelectedBeach, beachManager, _headIndex, _selectedFieldIndex));
+                if (_parkingBeaches != null && _parkingBeaches.Count != 0 && SelectedBeach != null) Engine.Instance?.UpdateScene(new ParkingBeachScene(SelectedBeach, _beachManager, _headIndex, _selectedFieldIndex));
                 break;
             case ConsoleKey.S:
-                // TODO: Logica de sorteo (voy a hacer un merge sort personalizado mergeSort(List<parkingBeach>, sortingState))
+                _headIndex = 0;
+                _selectedFieldIndex = 0;
+                _sortingState = (SortingState)(((int)_sortingState + 1) % Enum.GetValues(typeof(SortingState)).Length);
+                switch (_sortingState)
+                {
+                    case SortingState.capacity:
+                        _sortedBeaches = mergeSort(_parkingBeaches, _sortingState);
+                        break;
+                    case SortingState.revenue:
+                        _sortedBeaches = mergeSort(_parkingBeaches, _sortingState);
+                        break;
+                }
                 break;
 
-            // ACA TENES QUE HACER TU PARTE JOAQUIN
+            // ACA TENES QUE HACER TU PARTE JOAQUIN!!!!!!!!!!!!
             case ConsoleKey.C:
                 break;
             case ConsoleKey.E:
                 break;
             case ConsoleKey.D:
                 break;
-            // Yo recomiendo que hagas una variable Scene? newScene = TuScene y despues haces el update(newScene)
+             // Yo recomiendo que hagas una variable Scene? newScene = TuScene y despues haces el update(newScene)
 
         }
 
         // Selected Field Clamp
-        if (_parkingBeaches != null &&  _parkingBeaches.Count >= 4) _selectedFieldIndex = Math.Clamp(_selectedFieldIndex, 0, 3);
+        if (_parkingBeaches != null && _parkingBeaches.Count >= 4) _selectedFieldIndex = Math.Clamp(_selectedFieldIndex, 0, 3);
         else if (_parkingBeaches != null && _parkingBeaches.Count != 0) _selectedFieldIndex = Math.Clamp(_selectedFieldIndex, 0, _parkingBeaches.Count - 1);
+    }
+
+    /// <summary>
+    /// A method to sort the parking beach list with a mergeSort algorithm with an efficiency of O(n log n)
+    /// </summary>
+    /// <param name="list"></param>
+    /// <param name="state"></param>
+    /// <returns></returns>
+    private List<ParkingBeach> mergeSort(List<ParkingBeach> list, SortingState state)
+    {
+        if (list != null && list.Count > 1)
+        {
+            int middle = list.Count / 2;
+            List<ParkingBeach> L = list[0..middle]; // mitad izquierda
+            List<ParkingBeach> R = list[middle..list.Count]; // mitad derecha
+
+            //Ordenar las 2 mitades
+            mergeSort(L, state);
+            mergeSort(R, state);
+
+            int i = 0, j = 0, k = 0;
+
+            switch (state)
+            {
+                case SortingState.revenue: // revenue
+                    while (i < L.Count && j < R.Count)
+                    {
+                        if (L[i].TotalRevenue >= R[j].TotalRevenue)
+                        {
+                            list[k] = L[i];
+                            i += 1;
+                        }
+                        else
+                        {
+                            list[k] = R[j];
+                            j += 1;
+                        }
+                        k += 1;
+                    }
+                    while (i < L.Count)
+                    {
+                        list[k] = L[i];
+                        i += 1;
+                        k += 1;
+                    }
+                    while (j < R.Count)
+                    {
+                        list[k] = R[j];
+                        j += 1;
+                        k += 1;
+                    }
+                    break;
+
+                case SortingState.capacity: // capacity
+                    while (i < L.Count && j < R.Count)
+                    {
+                        if (L[i].FreeSlots <= R[j].FreeSlots)
+                        {
+                            list[k] = L[i];
+                            i += 1;
+                        }
+                        else
+                        {
+                            list[k] = R[j];
+                            j += 1;
+                        }
+                        k += 1;
+                    }
+
+                    while (i < L.Count)
+                    {
+                        list[k] = L[i];
+                        i += 1;
+                        k += 1;
+                    }
+                    while (j < R.Count)
+                    {
+                        list[k] = R[j];
+                        j += 1;
+                        k += 1;
+                    }
+                    break;
+            }
+
+        }
+        return list!;
     }
 }
